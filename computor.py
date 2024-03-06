e5ff15ca64aa2939acdb37ebfb81c1fb71b2caf0
artmende <artmende@student.s19.be>
2024-03-06 11:52:19 +0100

    still issues with exponents and coefficients

--- computor.py
+++ computor.py
@@ -13,11 +13,12 @@ import re
 # Display the reduced form
 # apply the math formula to get the discriminant, and to get the roots
 
-#coefficient can be followed by * or not
-#if no coefficient, it's 1
-
+# Examples of equations
 # 8 * x^2 + 7 * x^1 - 9.4 * X = 8x + 2 - 7x^2 + 3.3*x^0
 # x + 7x^2 - 6 = 4 * x^0 + 8 * x^1 - 8.1 + 43*x^2
+
+# ./computor.py "0x^2 + 4^02 + 8x + 9x^1 = 0" --- bug with this one
+
 
 def	main():
 	clean_input_array = args_check_and_treatment(sys.argv) # will exit the program if input is bad
@@ -150,6 +151,14 @@ def	check_for_common_errors(equation_string):
 	if re.search("[+\-.^][+\-.^]", equation_string) != None: # any 2 of those signs next to each other : [+-.^]
 		print("Problem with equation : 2 or more of those signs are next to each other : [+-.^]")
 		result = False
+	# instead, need to extract exponent value, and see if its outside of [0-2] range
+	search_result = re.findall("(?<=\^)-?[0-9]+", equation_string) # get the exponent value
+	for i in range(len(search_result)):
+		if int(search_result[i]) > 2 or int(search_result[i]) < 0:
+			print("Exponents cannot be lower than 0 or higher than 2")
+			result = False
+
+
 	if re.search("\^(?![0-2])", equation_string) != None: # exponent sign that is not followed by 0 - 1 - 2
 		print("Problem with equation : Each exponent sign --> ^ must be followed by one of those numbers [012]")
 		result = False
@@ -171,6 +180,10 @@ def	normalize_coeff_and_exponents(equation):
 		result = re.sub("(?<!\^)[0-9](?![x.0-9])", search_result[0] + "x^0", result, count=1) # Adding x^0 to numbers that are not an exponent, and that are not followed by x (or . or another number)
 		search_result = re.search("(?<!\^)[0-9](?![x.0-9])", result)
 	return result
+# need to treat exponents too ! 
+# replace exponents by their value 02 becomes 2
+
+
 
 if __name__ == "__main__":
 	main()
